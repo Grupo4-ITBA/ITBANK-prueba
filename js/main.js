cef8e97c3f6914cf22074b53b9afd861b69b9151
Florencia <sicref001@gmail.com>
2022-06-16 11:00:08 -0300

    new -v card

--- js/main.js
+++ js/main.js
@@ -2,12 +2,9 @@ const createElement = infoDollar => {
   const element = document.getElementById('newCard');
   element.innerHTML += `   
   <div
-  class="align-items-center w-40 mt-4 mh-20"
-  data-aos="zoom-in"
-  data-aos-delay="300"
+  class="col-lg-4 mt-2"
 >
   <div class="icon-box">
-    <div class="icon"><i class="bx bx-tachometer"></i></div>
     <h4><a href="">${infoDollar.nombre}</a></h4>
     <small class="buy mb-1"><strong>Compra:</strong>${infoDollar.compra}</small>
     <small class="sell"><strong>Venta:</strong> ${infoDollar.venta}</small>
@@ -21,7 +18,6 @@ function getInformation() {
     .then(data => {
       document.getElementById('loading').hidden = true;
       data.forEach(element => {
-        console.log(element.casa);
         if (element.casa.compra != 'No Cotiza') {
           createElement(element.casa);
         }
