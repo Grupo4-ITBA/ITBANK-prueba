a6966734ecb8d1a0da778a625bc2d7ba3132af92
Lorenzo Lara <lorenzolara12@gmail.com>
2022-06-16 13:07:54 -0300

    Cambios al layout y elementos html de la coti

--- js/main.js
+++ js/main.js
@@ -1,15 +1,47 @@
+let i = 0;
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+today = dd + '/' + mm + '/' + yyyy;
+
+getInformation();
+
 const createElement = infoDollar => {
   const elementList = document.getElementById('list-group');
   elementList.innerHTML += ` 
-  <div class="col ">
-  <div class="card">
-    <div class="card-body">
-      <h5 class="card-title">${infoDollar.nombre}</h5>
-      <small class="buy mb-1"><strong>Compra:</strong>${infoDollar.compra}</small>
-      <small class="sell"><strong>Venta:</strong> ${infoDollar.venta}</small>
-    </div>
-  </div>
-</div>`;
+    <div class="col ">
+      <div class="card">
+        <div class="card-header text-bg-dark mb-3">
+        ${infoDollar.nombre}
+        </div>
+        <div class="card-body">
+        <div class="row">
+          <div class="col-8" >
+              <p>Compra: ${infoDollar.compra} </p> 
+              <p>Venta: ${infoDollar.venta} </p> 
+            </div>
+            <div class="col-1  w-auto" id="cotiz${i}">
+            </div>
+          </div>
+        </div>
+        <div class="card-footer text-muted">
+            Ultima Consulta: ${today}
+          </div>
+      </div>
+    </div>`;
+  
+    const cotizDolar = document.createElement("p");
+  if( infoDollar.variacion.includes("-")){
+    cotizDolar.classList.add("text-danger");
+    cotizDolar.textContent = infoDollar.variacion;
+  }else{
+    cotizDolar.classList.add("text-success");
+    cotizDolar.textContent = infoDollar.variacion;
+  }
+  const divCotiz = document.getElementById("cotiz"+i);
+  divCotiz.appendChild(cotizDolar);
+  i++;
 };
 
 function getInformation() {
@@ -26,4 +58,3 @@ function getInformation() {
     });
 }
 
-getInformation();
