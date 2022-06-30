5b892ebccac99a9d3e8652cb1464ce3c7ccc8699
Florencia <sicref001@gmail.com>
2022-06-30 14:55:48 -0300

    get data from sys.argv

--- Python/listado_chesques.py
+++ Python/listado_chesques.py
@@ -1,24 +1,32 @@
 from asyncio.windows_events import NULL
+import sys 
 import csv
 from dataclasses import Field
 from datetime import *
 
-archivo = input("Ingrese la ruta del archivo que quiere leer.. ")
+
+
+#archivo = input("Ingrese la ruta del archivo que quiere leer.. ")
 listAll = input("Quire listar todos los cheques? (Y/N)...")
 if listAll == 'N':
-    dni = input("Ingrese su DNI... ")
-    tipoC = input("EMITIDO o DEPOSITADO?.. ")
-    estadoC = input("Estado del cheque? (PENDIENTE/APROBADO/RECHAZADO).. ")
+    dni = sys.argv[1]
+    #dni = input("Ingrese su DNI... ")
+    tipoC = sys.argv[4]
+    #tipoC = input("EMITIDO o DEPOSITADO?.. ")
+    estadoC = sys.argv[5]
+    #estadoC = input("Estado del cheque? (PENDIENTE/APROBADO/RECHAZADO).. ")
     # d1, m1, y1 = [int(x) for x in input("Fecha desde (ex. 01-April-2021).. ").split('-')]
     # d2, m2, y2 = [int(x) for x in input("Fecha hasta (ex. 01-April-2021).. ").split('-')]
-    dateFro = input("Fecha desde (ex. 01-April-2021).. ")
-    dateTo = input("Fecha hasta (ex. 01-April-2021).. ")
+    dateFro = sys.arg[2]
+    #dateFro = input("Fecha desde (ex. 01-April-2021).. ")
+    dateTo = sys.argv[3]
+    #dateTo = input("Fecha hasta (ex. 01-April-2021).. ")
     # dateT = input("Fecha desde (ex. 01-April-2021).. ")
     # if d1 != '' and d2 != '':
     #     dateFro = date(y1, m1, d1)
     #     dateTo = date(y2, m2, d2)
 
-with open(archivo, mode="r") as csv_file:
+with open(sys.argv, mode="r") as csv_file:
     csv_reader = csv.DictReader(csv_file)
     reader_line = 0
     if reader_line == 0:
